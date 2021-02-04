d89d4dba31a126a155ad673448d30034497b43c1
alikhanmohammadsajid <alikhanmohammadsajid@gmail.com>
2021-02-04 19:42:44 +0600

    null vs undefined clear

---
+++ null-vsundefind.js
@@ -0,0 +1,16 @@
+ let pakhi;
+ console.log(pakhi);
+ function add(num1, num2) {
+     console.log(num1 + num2);  
+}
+const result = add(13, 87)
+ console.log(result);
+ function add(num1, num2) {
+     console.log(num1, num2);  
+ }
+ const result = add(13)
+ console.log(result);
+ const name = {name:'sajid', roll :'87', phone:'758345837458'}
+ console.log(name.gf)
+let fun = undefined;
+console.log(fun)
---
+++ truefalse.js
@@ -0,0 +1,34 @@
+// falsy:
+// 0
+// false
+// ''
+// Nan
+// null
+// undefined
+// truthy
+// ' '
+// '0'
+// []
+// {}
+
+
+
+// const roll = '12';
+// const name = 'sajid';
+// const num = 0;
+
+
+// if (num) {
+//     console.log('condition is true')
+// }
+// else{
+//     console.log('condition is false')
+// }
+
+let name = 12;
+if (name || name == 0) {
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
